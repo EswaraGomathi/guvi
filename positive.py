f4853a219982efbbf7065fadf37e62a44e9f2d20
EswaraGomathi <prettylakshmi1997@gmail.com>
2017-06-23 13:51:08 +0530

    Create positive.py

---
+++ positive.py
@@ -0,0 +1,10 @@
+def pos():
+        n=('enter a no')
+        if(n>0)
+          print('the no is positive')
+        if(n<0)
+          print('the no is negative')
+        if(n=0)
+          print('zero')
+        return
+pos()    
